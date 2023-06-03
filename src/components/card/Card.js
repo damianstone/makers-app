c5551d9c278371a9b8f0b205e5ecb4dfcbdba4b7
Damian Stone <damianstonedev@gmail.com>
2023-06-03 19:15:55 +0100

    fix: styles when rending cards (companies)

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,49 +2,65 @@ import React from 'react';
 
 import './Card.css';
 
-const Card = () => {
+const Card = ({
+  companyPhoto,
+  profilePhoto,
+  firstname,
+  lastname,
+  position,
+  companyName,
+  companyDescription,
+  interestsList,
+  valuation,
+  numberEmployees,
+  lastInvestment,
+}) => {
   return (
     <div className='card'>
-      <div className='companyPhoto'>COMPANY PHOTO</div>
+      <div className='companyPhoto'>
+        <div className='companyPhotoContainer'>
+          <img className='companyImageStyle' src={companyPhoto} alt='Profile' />
+        </div>
+      </div>
       <div className='cardContent'>
         <div className='profileInfo'>
-          <div className='profilePhotoContainer'>IMAGE</div>
-          <p className='profileName'>Damian Stone</p>
-          <p className='position'>CEO at Toogether</p>
+          <div className='profilePhotoContainer'>
+            <img
+              className='profileImageStyle'
+              src={profilePhoto}
+              alt='Profile'
+            />
+          </div>
+          <p className='profileName'>{`${firstname} ${lastname}`}</p>
+          <p className='position'>{position}</p>
         </div>
         <div className='textInfoContainer'>
           <div className='companyDescriptionContainer'>
-            <p className='companyName'>Toogether</p>
-            <p className='companyDescription'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              sagittis, odio eu auctor euismod, ligula leo viverra odio, eget
-              sodales ex orci in purus. Donec a tortor finibus, pharetra nisi
-              ut, posuere lectus. In hac habitasse platea dictumst. Morbi nec
-              tristique dolor. Integer nec leo est. Nunc id libero commodo.
-            </p>
+            <p className='companyName'>{companyName}</p>
+            <p className='companyDescription'>{companyDescription}</p>
           </div>
           <div className='interestsContainer'>
             <p className='interestTitle'>Interests</p>
             <div className='interestsList'>
-              <p>Interest 1, </p>
-              <p>Interest 2, </p>
-              <p>Interest 3</p>
+              {interestsList.map((int) => (
+                <p>{`${int}, `}</p>
+              ))}
             </div>
           </div>
         </div>
         <div className='companyInfoContainer'>
           <div className='companyInfoWrapper'>
             <div className='companyInfo'>
-              <p>Valuation: </p>
-              <p> 2</p>
+              <p>Number of employees: </p>
+              <p> {numberEmployees}</p>
             </div>
             <div className='companyInfo'>
-              <p>Valuation: </p>
-              <p> 2</p>
+              <p>Valuation:</p>
+              <p>{`$${valuation} USD`}</p>
             </div>
             <div className='companyInfo'>
-              <p>Valuation: </p>
-              <p> 2</p>
+              <p>Last investment: </p>
+              <p>{`$${lastInvestment} USD`}</p>
             </div>
           </div>
           <button className='requestButton'>Request meeting</button>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import { CARDS } from '../../data/cards';
 import Navbar from '../../components/navbar/Navbar';
 import SideBar from '../../components/sidebar/SideBar';
 import Card from '../../components/card/Card';
@@ -26,7 +27,21 @@ const Home = () => {
         <SideBar navigate={navigate} handleFilter={handleFilter} />
         <div className='cards'>
           <p className='cardsTitle'>Startups - Agriculture</p>
-          <Card />
+          {CARDS.map((obj) => (
+            <Card
+              companyPhoto={obj.company_photo}
+              profilePhoto={obj.photo}
+              firstname={obj.firstname}
+              lastname={obj.lastname}
+              position={obj.position}
+              companyName={obj.company_name}
+              companyDescription={obj.company_description}
+              interestsList={obj.interests}
+              valuation={obj.company_valuation}
+              numberEmployees={obj.company_employees}
+              lastInvestment={obj.company_investment}
+            />
+          ))}
         </div>
       </div>
     </div>
